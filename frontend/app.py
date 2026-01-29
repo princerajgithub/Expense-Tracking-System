336f3171d81ed9910588d496903b93f1daf4feb0
Prince Raj Singh <125367546+princerajgithub@users.noreply.github.com>
2026-01-29 12:06:34 +0530

    Add files via upload

---
+++ frontend/app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from add_update_ui import add_update_tab
+from analytics_ui import analytics_tab
+from analytics_by_month import analytics_by_month_tab
+
+st.title("Expense Management System")
+
+tab1,tab2, tab3 = st.tabs(["Add/Update", "Analytics","Analytics by Month"])
+
+with tab1:
+    add_update_tab()
+
+with tab2:
+    analytics_tab()
+
+with tab3:
+    analytics_by_month_tab()
